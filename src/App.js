eaa520df03fc9b3cae98506caa66964b3c0f2b64
ntuvera <ntuvera@gmail.com>
2021-12-14 14:46:22 -0500

    finally a working version of react router v5 vs v6 learning issues

--- src/App.js
+++ src/App.js
@@ -1,39 +1,37 @@
 import React from 'react'
 import styled from 'styled-components';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css';
 
 import Main from './components/Main';
 import Header from './components/layout/Header';
 import Content from './components/layout/Content';
-import Roster from './components/Roster';
 import Footer from './components/layout/Footer';
+import Home from './pages/Home';
+import NewsPage from './pages/NewsPage';
+import Roster from './components/Roster';
 
 function App() {
   return (
     <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/news" element={<NewsPage />}/>
+          <Route path="/roster" element={<Roster />}/>
+        </Routes>
+      </Router>
       <Container className="App">
-        <Header></Header>
-          <Content>
-            <Roster/>
-          </Content>
-          <Main/>
+        <Header/>
+        <Content/>
+        <Footer/>
       </Container>
-      <Footer/>
     </>
   )
 }
 
 export default App;
 
-
-// const Container = styled.div`
-//   min-width: 440px;
-//   margin: 0 auto;
-
-//   @media (min-width: 480px) {
-//     max-width: 1025px;
-//   }
-// `
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,17 +1,10 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-
-import Home from '../pages/Home';
-import NewsPage from '../pages/News';
-import RosterPage from '../pages/Roster';
 
 const Main = () => {
   return (
-    <Routes>
-      <Route exact path='/' component={Home}></Route>
-      <Route exact path='/news' component={NewsPage}></Route>
-      <Route exact path='/roster' component={RosterPage}></Route>
-    </Routes>
+    <>
+      <h1>main.jsx</h1>
+    </>
   );
 }
 
